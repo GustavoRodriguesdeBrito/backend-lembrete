e9213ed7bc419362fc21a39aa298976b58a165bf
Gustavo Rodrigues de Brito <gustavorodriguesdebrito@outlook.com>
2020-11-16 23:56:14 -0300

    Funcionalidade de deleção e edição de registro adicionada; remoção das funcionalidades de conversão de data (futuramente serão implementadas no frontend)

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 ///* inicialização de variávies do ambiente
 const dotenv = require('dotenv');
-const result = dotenv.config({path:'./config/config.env'});
-if(result.error) throw result.error; ///! o sistema não pode prosseguir sem essas variáveis
-console.log("=== variáveis do ambiente carregadas ===\n",result.parsed);
+const result = dotenv.config({ path: './config/config.env' });
+if (result.error) throw result.error; ///! o sistema não pode prosseguir sem essas variáveis
+console.log('=== variáveis do ambiente carregadas ===\n', result.parsed);
 
 const app = express();
 app.use(express.json()); ///* body parser
 mongoose
-    .connect(
-        process.env.MONGODB_CON_STR,
-        { useNewUrlParser: true, useUnifiedTopology: true }
-    )
+    .connect(process.env.MONGODB_CON_STR, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => {
         console.log('conexão com MongoDB Atlas estabelecida');
     })
@@ -38,40 +38,91 @@ app.use((req, res, next) => {
 });
 
 app.get('/lembrete', (req, res, next) => {
-    Lembrete.find().then((lembretes) => {
-      console.log("lembretes: \n", lembretes);
-      ///* A maneira com a qual o frontend interpreta os dados requer que eles sejam enviados em um vetor sem chaves no json
-      ///* similar a "let lembretes = [{lembr1},{lembr2}...];"
-      res.status(200).json(lembretes);
-    })
-    .catch((err) => {
-      console.error("\n=== ERRO ===\n", err);
-      res.status(500).json({msg:"problemas ao consultar o banco de dados"});
-    });
+    Lembrete.find()
+        .then((lembretes) => {
+            ///* A maneira com a qual o frontend interpreta os dados requer que eles sejam enviados em um vetor sem chaves no json
+            ///* similar a "let lembretes = [{lembr1},{lembr2}...];"
+            res.status(200).json(lembretes);
+        })
+        .catch((err) => {
+            console.error('\n=== ERRO DE CONSULTA ===\n', err);
+            res.status(500).json({
+                msg: 'erro ao consultar o banco de dados',
+            });
+        });
 });
 
 app.post('/lembrete', (req, res, next) => {
     const lembrete = new Lembrete({
         conteudo: req.body.conteudo,
-        //converter as datas do formato 'dd/mm/yyyy' para o formato 'yyyy-mm-dd'
-        dataCriado: moment(req.body.dataCriado, "DD/MM/YYYY").format("YYYY-MM-DD"),
-        prazoFinal: moment(req.body.prazoFinal, "DD/MM/YYYY").format("YYYY-MM-DD"),
+        ///* As datas precisam estar no formato 'yyyy-mm-dd'/ISO
+        dataCriado: moment().utc(true).toDate(), ///* inserir data de modificação mantendo a hora local
+        prazoFinal: req.body.prazoFinal,
         arquivado: req.body.arquivado,
-        prioridade: req.body.prioridade
+        prioridade: req.body.prioridade,
     });
-    lembrete.save()
-    .then((lembreteCadastrado) => {
-        console.log("Cadastrando item: ", lembreteCadastrado);
-        res.status(201).json(lembreteCadastrado);
-    })
-    .catch((err) => {
-        console.error("=== ERRO DE CADASTRO ===\n", err);
-        res.status(500).json({msg:'erro de cadastro'});
-    });
+    lembrete
+        .save()
+        .then((lembreteCadastrado) => {
+            res.status(201).json(lembreteCadastrado);
+        })
+        .catch((err) => {
+            console.error('=== ERRO DE CADASTRO ===\n', err);
+            res.status(500).json({ msg: 'erro ao cadastrar' });
+        });
 });
 
-app.put('/lembrete/:id', (req, res, next) => {});
+app.put('/lembrete/:id', (req, res, next) => {
+    ///* "lembreteNovo" representa os novos dados que o cliente cedseja inserir no banco de dados
+    const lembreteNovo = {
+        conteudo: req.body.conteudo,
+        dataCriado: req.body.dataCriado,
+        prazoFinal: req.body.prazoFinal,
+        arquivado: req.body.arquivado,
+        prioridade: req.body.prioridade,
+        modificado: moment().utc(true).toDate(), ///* inserir data de modificação mantendo a hora local
+    };
+    Lembrete.updateOne({ _id: req.params.id }, lembreteNovo)
+        .then((result) => {
+            if (result.n === 0) {
+                res.status(404).json({
+                    msg: `registro ${req.params.id} não encontrado`,
+                });
+            } else {
+                res.status(200).json({
+                    msg: `registro ${req.params.id} atualizado`,
+                });
+            }
+        })
+        .catch((err) => {
+            console.error('=== ERRO AO ALTERAR ===\n', err);
+            res.send(500).json({
+                msg: `erro ao alterar registro ${req.params.id}`,
+            });
+        });
+});
 
-app.delete('/lembrete/:id', (req, res, next) => {});
+app.delete('/lembrete/:id', (req, res, next) => {
+    Lembrete.deleteOne({ _id: req.params.id })
+        .then((result) => {
+            ///* "result.n" guarda a quantidade de registros deletados.
+            ///* Se for 0, significa que nenhum registro foi deletado.
+            if (result.n === 0) {
+                res.status(404).json({
+                    msg: `registro ${req.params.id} não encontrado`,
+                });
+            } else {
+                res.status(200).json({
+                    msg: `registro ${req.params.id} deletado`,
+                });
+            }
+        })
+        .catch((err) => {
+            console.error('=== ERRO AO DELETAR ===\n', err);
+            res.send(500).json({
+                msg: `erro ao deletar registro ${req.params.id}`,
+            });
+        });
+});
 
 module.exports = app;
